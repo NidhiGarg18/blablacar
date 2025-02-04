9488c4d15ea50165f21ffdb27a1976c13d5ebfeb
NidhiGarg18 <112054725+NidhiGarg18@users.noreply.github.com>
2025-02-04 19:23:06 +0530

    Add files via upload

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -20,96 +20,130 @@ class _HomepageState extends State<Homepage> {
               child: SingleChildScrollView(
                 // child: Column(
                 //   children: [
-                    child:ConstrainedBox(
-                      constraints: const BoxConstraints(
-                        minHeight: 800,
-                      ),
-                    
-                      //height: 700,
-                      child: Stack(
-                        children: [
-                          // Column(
-                          //   children: [
-                          Container(
-                            height: 500,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: AssetImage("assets/img_6.jpg"), // Replace with your image
-                                fit: BoxFit.cover,
-                              ),
-                            ),),
-                            Positioned(
-                              top: 100,
-                              left: 20,
-                              right: 20,
-                             child:Column(
-                              children: [
-                               Text(
-                                "Your pick of rides at low prices",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                  textAlign: TextAlign.center,
-                                ),   
-                              ],
-                            ),
-                              ),
+                    child:Container(
+                      color: Color(0xFF1A1A1A),
+                      child: ConstrainedBox(
+                        constraints: const BoxConstraints(
+                          minHeight: 800,
+                          
+                        ),
+                      
+                        //height: 700,
+                        child: Stack(
+                          children: [
+                            
+                            // Column(
+                            //   children: [
+                            Container(
+                              
+                              height: 500,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage("assets/img_6.jpg"), // Replace with your image
+                                  fit: BoxFit.cover,
+                                ),
+                              ),),
                               Positioned(
-                                top: 300,
+                                top: 100,
                                 left: 20,
                                 right: 20,
-                                child: Container(
-                                  color: Colors.white,
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(30),
-                                    child: Column(
-                                      children: [
-                                        TextField(
-                                          decoration: InputDecoration(
-                                            hintText: "Leaving from",
-                                            icon: Icon(Icons.radio_button_checked),
-                                            ),),
+                               child:Column(
+                                children: [
+                                 Text(
+                                  "Your pick of rides at low prices",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),   
+                                ],
+                              ),
+                                ),
+                                Positioned(
+                                  top: 300,
+                                  left: 20,
+                                  right: 20,
+                                  child: Container(
+                                    //color: Colors.black,
+                                    //child: Padding(
+                                      //padding: const EdgeInsets.all(10),
+                                       decoration: BoxDecoration(
+                                       color: Color(0xFF333333), 
+                                       borderRadius: BorderRadius.circular(15), // Rounded corners
+                                        ),
+                                      child: Column(
+                                        children: [
+                                          Container(
+                                            padding: EdgeInsets.all(20),
+                                           child:  Column(children: [
                                             TextField(
-                                              decoration: InputDecoration(
-                                                hintText: "Going to",
-                                                icon: Icon(Icons.radio_button_checked),
-                                                ),),
-                                                TextField(
-                                                  decoration: InputDecoration(
-                                                    hintText: "Today",
-                                                    icon: Icon(Icons.date_range),
-                                                    ),),                              
-                                                    TextField(
-                                                      decoration: InputDecoration(
-                                                        hintText: "No. of person",
-                                                        icon: Icon(Icons.person),
-                                                        ),),
-                                                        SizedBox(height: 20,),
-                                                        ElevatedButton(onPressed: (){},style: ElevatedButton.styleFrom(
-                                                          backgroundColor: Colors.blue,
-                                                          shape: RoundedRectangleBorder(
-                                                            borderRadius: BorderRadius.circular(8),
+                                            decoration: InputDecoration(
+                                              hintText: "Leaving from",
+                                              hintStyle: TextStyle(color: Color(0xFF1A1A1A)),       
+                                              icon: Icon(Icons.radio_button_checked,color: Color(0xFF1A1A1A),),
+                                              ),),
+                                              TextField(
+                                                decoration: InputDecoration(
+                                                  hintText: "Going to",
+                                                  hintStyle: TextStyle(color: Color(0xFF1A1A1A)),
+                                                  icon: Icon(Icons.radio_button_checked,color: Color(0xFF1A1A1A),),
+                                                  ),),
+                                                  TextField(
+                                                    decoration: InputDecoration(
+                                                      hintText: "Today",
+                                                      hintStyle: TextStyle(color: Color(0xB3FFFFFF)),
+                                                      icon: Icon(Icons.date_range,color: Color(0xFF1A1A1A),),
+                                                      ),),                              
+                                                      TextField(
+                                                        decoration: InputDecoration(
+                                                          hintText: "No. of person",
+                                                          hintStyle: TextStyle(color: Color(0xB3FFFFFF)),
+                                                          icon: Icon(Icons.person,color: Color(0xFF1A1A1A),),
+                                                          ),),
+
+                                            ],)
+                                          ),
+                      
+                                          
+                                                          // SizedBox(height: 20,),
+                                                          Padding(
+                                                            padding: const EdgeInsets.all(1),
+                                                            child: Container(
+                                                              child: SizedBox(
+                                                                width: double.infinity,
+                                                                child: ElevatedButton(onPressed: (){},style: ElevatedButton.styleFrom(
+                                                                  backgroundColor: Color(0xFF1E88E5),
+                                                                  shape: ContinuousRectangleBorder(
+                                                                    borderRadius: BorderRadius.circular(20),
+                                                                    ),
+                                                                    padding: EdgeInsets.symmetric(vertical: 14,horizontal: 100),
+                                                                    ),child: Text("Search",style: TextStyle(color: Colors.white,fontSize: 20),),),
+                                                              ),
                                                             ),
-                                                            padding: EdgeInsets.symmetric(vertical: 14,horizontal: 100),
-                                                            ),child: Text("Search",style: TextStyle(color: Colors.white,fontSize: 20),),),
-                                                            ],),
-                                                            ),
-                                                        
-                                                        ),
-                                                        ),
-                                                          ],
                                                           ),
-              
+                      
+                                      //),
+                      
+                                                              ],),
+                                                             // ),
                                                           
-                                                      ),
+                                                          ),
+                                                          ),
+                                                            ],
+                                                            ),
+                                    
+                                                            
+                                                        ),
+                    ),
               ),
             ),
 
             bottomNavigationBar: BottomNavigationBar(
-         selectedItemColor: Colors.blue,
-         unselectedItemColor: Colors.black54,
+             
+         selectedItemColor: Color(0xFF2196F3),
+         unselectedItemColor: Color(0x8A000000),
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
           BottomNavigationBarItem(icon: Icon(Icons.add), label: "Publish"),
